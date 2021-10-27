1a5c966386e52f328715f500acf3c16120ef1e11
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2021-10-27 12:06:14 -0400

    Update edgeunity-skipper.js

--- assets/vendor/edgeunity/edgeunity-skipper.js
+++ assets/vendor/edgeunity/edgeunity-skipper.js
@@ -40,7 +40,7 @@ function injectoverlay() {
         $("#skipper-overlay").append($('<div id="skipper-text">edgenuity-skipper<br /></div>')),
         $("#skipper-text").append($("<input id='autoplay-checkbox' type='checkbox' onchange='autoplay_checkbox()'></input><label for='autoplay-checkbox'>Autoplay</label><br />")),
         ($("#autoplay-checkbox")[0].checked = !0),
-        $("#skipper-text").append($("<input id='intro-skip' type='checkbox' onchange='audio_skip_update(this,\"entry\");alert(window.skipperSettings);'></input><label for='intro-skip'>Skip intro audio</label>")),
+        $("#skipper-text").append($("<input id='intro-skip' type='checkbox' onchange='audio_skip_update(this,\"entry\")'></input><label for='intro-skip'>Skip intro audio</label>")),
         ($("#intro-skip")[0].checked = !0),
         $("#skipper-text").append($("<input id='hint-skip' type='checkbox' onchange='audio_skip_update(this,\"hint\")'></input><label for='hint-skip'>Skip hint audio</label>")),
         ($("#hint-skip")[0].checked = !0),
@@ -56,6 +56,10 @@ function injectoverlay() {
 function audio_skip_update(e, t) {
     skipperSettings.skip[t] = e.checked;
 }
+audio_skip_update(this, entry);
+audio_skip_update(this, hint);
+audio_skip_update(this, exit);
+
 function audio_blocker() {
     API.Audio.playAudioInner = new Proxy(API.Audio.playAudioInner, {
         apply: function (e, t, i) {
@@ -95,7 +99,7 @@ function init() {
 (window.API = document.querySelector("#stageFrame").contentWindow.API),
     (window.skipperSettings = {}),
     (skipperSettings.autoplay = !0),
-    (skipperSettings.skip = !0),
+    (skipperSettings.skip = {}),
     (API.Video.videoDone = new Proxy(API.Video.videoDone, {
         apply: debounce(function (e, t, i) {
             e.apply(t, i), skipperSettings.autoplay && setTimeout(API.FrameChain.nextFrame, 100);
