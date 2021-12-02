97f5b43da114f890378a402d7017bdb4eb4f40fa
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2021-12-02 16:50:19 -0500

    Update edgeunity-skipper-v2.js

--- assets/vendor/edgeunity/edgeunity-skipper-v2.js
+++ assets/vendor/edgeunity/edgeunity-skipper-v2.js
@@ -257,40 +257,36 @@ const R=['0x18','[class*=\x27wysiwyg_frame\x27]','0x80','0x21','0x1','parentElem
 
 /* override original buttons */
 
-let $;
-let
-    jQuery;
+var $, jQuery;
 $ = jQuery = window.jQuery;
 
 function main() {
-    function buildMenuButton(text, id, event, overrideid) { // puts buttons in the menu
-        let x;
-        let
-            y;
-        y = document.createElement('li');
-        y.id = id;
-        y.append(x = document.createElement('button'));
-        x.style = 'display:block; padding: 0; border: 2px; background: none; height:45px; width:54px; background-color: rgb(51, 51, 51); color: rgb(249, 166, 25); opacity: 1;';
-        x.innerText = text;
-        x.addEventListener('click', event);
+    function buildMenuButton(text, id, event, overrideid) { //puts buttons in the menu
+        var x, y;
+        y = document.createElement("li")
+        y.id = id
+        y.append(x = document.createElement("button"))
+        x.style = "display:block; padding: 0; border: 2px; background: none; height:45px; width:54px; background-color: rgb(51, 51, 51); color: rgb(249, 166, 25); opacity: 1;"
+        x.innerText = text
+        x.addEventListener("click", event);
         if (!overrideid) {
-            $('.toolbar')[0].append(y);
-        } else $(`#${overrideid}`).append(y);
-    }
-}
-alert('creating the function for button finished');
-buildMenuButton('Tweaks Menu', 'tweaksbutton', () => {
-    if (document.getElementById('tweaksmenu').style.visibility == 'hidden') {
-        document.getElementById('tweaksmenu').style.visibility = 'visible';
+            $(".toolbar")[0].append(y);
+        } else $("#" + overrideid).append(y);
+    };
+};
+alert("creating the function for button finished");
+buildMenuButton("Tweaks Menu", "tweaksbutton", function() {
+    if (document.getElementById("tweaksmenu").style.visibility == "hidden") {
+        document.getElementById("tweaksmenu").style.visibility = "visible"
     } else {
-        document.getElementById('tweaksmenu').style.visibility = 'hidden';
+        document.getElementById("tweaksmenu").style.visibility = "hidden"
     }
 });
 init();
-alert('button placed');
-const calcButton = document.getElementById('tools-calc');
-const resButton = document.getElementById('tools-res');
-const tweaksButton = document.getElementById('tweaksbutton');
+alert("button placed");
+var calcButton = document.getElementById("tools-calc");
+var resButton = document.getElementById("tools-res");
+var tweaksButton = document.getElementById("tweaksbutton");
 tweaksButton.classList.add('tools-headphones1', 'audio');
 resButton.remove();
 calcButton.remove();
