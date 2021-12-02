f298086ab996168b12916d4c09331bf686e992dc
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2021-12-02 17:07:11 -0500

    Update course-unlocker.js

--- assets/vendor/edgeunity/course-unlocker.js
+++ assets/vendor/edgeunity/course-unlocker.js
@@ -1,20 +1,241 @@
 var $, jQuery;
 $ = jQuery = window.jQuery;
 
-function main() {
-    function buildMenuButton(text, id, event, overrideid) { //puts buttons in the menu
-        var x, y;
-        y = document.createElement("li")
-        y.id = id
-        y.append(x = document.createElement("button"))
-        x.style = "display:block; padding: 0; border: 2px; background: none; height:45px; width:54px; background-color: rgb(51, 51, 51); color: rgb(249, 166, 25); opacity: 1;"
-        x.innerText = text
-        x.addEventListener("click", event);
-        if (!overrideid) {
-            $(".toolbar")[0].append(y);
-        } else $("#" + overrideid).append(y);
-    };
-};
+setTimeout( //2 sec delay to load before trying to run
+    function main() {
+
+        console.log("HelloWorldEdgenTweaks")
+
+        function init() {
+            window.openedWindows = [];
+            window.configElements = []; //Config infomation
+            try {
+                $(".mainfoot")[0].append(null);
+            } //stupid hack to prevent it building twice
+            catch {
+                console.log('%cIGNORE THE ERROR BELOW ME!', 'color: green; font-weight: bold;');
+            $(".mainfoot")[0].append(null);
+        }
+
+        //Internal Functions
+        function buildMenuButton(text, id, event, overrideid) { //puts buttons in the menu
+            var x, y;
+            y = document.createElement("li")
+            y.id = id
+            y.append(x = document.createElement("button"))
+            x.style = "display:block; padding: 0; border: 2px; background: none; height:45px; width:54px; background-color: rgb(51, 51, 51); color: rgb(249, 166, 25); opacity: 1;"
+            x.innerText = text
+            x.addEventListener("click", event);
+            if (!overrideid) {
+                $(".toolbar")[0].append(y);
+            } else $("#" + overrideid).append(y);
+        }
+
+        function RenderPane(name, id, width, height, transform) { //renders panes
+            window.pane = document.createElement("div")
+            window.pane.style = "padding:5px; z-index:1; overflow:auto; visibility: hidden; position: fixed; border: 1px solid rgb(95, 95, 95); background-color: rgb(39, 39, 39); color: rgb(249, 166, 25); text-align: center; top:50%;left:50%; transform: translate(-50%, -50%)"
+            window.pane.style.width = width
+            window.pane.style.height = "auto"
+            window.pane.style.maxHeight = height;
+            //window.pane.style.marginTop = -(height/2)
+            //window.pane.style.marginLeft = -(width/2)
+            window.pane.id = id;
+            window.pane.classList.add("tweakpane")
+            x = document.createElement("header")
+            x.style = ""
+            var button = document.createElement("img");
+            button.src = "https://cdn.pixabay.com/photo/2012/04/12/20/12/x-30465_1280.png";
+            button.style = "filter: sepia(1);;position:sticky;opacity:60%;text-align:left;left:0;top:0;height:15px;width:15px";
+            button.align = "left"
+            button.onclick = function() {
+                $("#" + id).css("visibility", "hidden") //find my pane and close
+                $("#" + id + "~ div[id]").css("visibility", "hidden")
+            }; //find panes after me and close
+            x.appendChild(button);
+            var z = document.createElement("label")
+            z.style = " float: right;"
+            z.innerText = "info"
+            var y = document.createElement("input")
+            y.type = "checkbox";
+            y.checked = "true";
+            y.style = "margin: 3px; float: right";
+            y.addEventListener("click", function() {
+                $("#" + id + " p").fadeToggle()
+                // $("#" +id).css("height", "auto")
+            })
+            x.appendChild(y)
+            x.appendChild(z);
+            window.panetitle = document.createElement("h1")
+            window.panetitle.innerText = "     " + name //shitty centering fix
+            x.appendChild(window.panetitle)
+            window.pane.appendChild(x);
+            $("#overlay").append(window.pane)
+        }
+
+        function BuildMenuEntry(name, info, id, configpane, override, element) { //Creates tickbox element with info and optional (new) config pane (see guesspractice). in a pane
+            //Override 1 append to tweak menu
+            //Override 2 no text no check just button and append
+            //Override 3 no button
+            window.entry = document.createElement("div")
+            window.entry.margin = "5px"
+            // window.entry.onmouseleave =function(){setTimeout(function(){$("#" + id +"~ p").fadeOut()}, 1000);}
+            if (override != 2 && override != 3) {
+                window.tickbox = document.createElement("input")
+                window.tickbox.type = "checkbox"
+                window.tickbox.id = id
+                window.configElements.push(id)
+                window.entry.appendChild(window.tickbox)
+            }
+            window.label = document.createElement("label");
+            window.label.innerText = " " + name //spacing fix
+            window.entry.appendChild(window.label)
+            if (configpane != undefined) { //If any configpane was passed through try and create a button to open it.
+                window.configbutton = document.createElement("button")
+                window.configbutton.innerText = "Configure"
+                window.configbutton.style.marginLeft = "7px"
+                window.configbutton.style.border = "1px solid #5f5f5f"
+                window.configbutton.style.boxShadow = "inset 0 0 5px rgba(0, 0, 0, 0.6)"
+                window.configbutton.style.backgroundColor = "rgb(39, 39, 39)"
+                window.configbutton.style.color = "#f9a619"
+                window.configbutton.style.borderRadius = "3px"
+                window.configbutton.onclick = function() {
+                    if (document.getElementById(configpane).style.visibility == "hidden") { //visiblitly handler for configpane button
+                        document.getElementById(configpane).style.visibility = "visible"
+                    } else {
+                        document.getElementById(configpane).style.visibility = "hidden"
+                    }
+                }
+                window.entry.appendChild(window.configbutton)
+            }
+            if (element != null) {
+                window.entry.appendChild(element);
+            }
+            if (info != "") {
+                window.desc = document.createElement("p")
+                window.desc.innerHTML = info;
+                window.desc.style = "position: abosolute;";
+                window.entry.appendChild(window.desc)
+            }
+            //   window.label.onmouseenter = function () {
+            //  $("#" + id +"~ p").siblings("p").hide();
+            //  $("#" + id +"~ p").fadeIn()}
+            //
+            if (override == 1) { //override
+                $("#tweaksmenu").append(window.entry);
+            }
+            if (override == 2) {
+                window.configbutton.innerText = "Search Selection Config"
+                $("#tweaksmenu").append(window.entry);
+            } else return window.entry;
+        }
+
+        function dragElement(elmnt) {
+            //drag elemments
+            //https://www.w3schools.com/howto/howto_js_draggable.asp
+            var pos1 = 0,
+                pos2 = 0,
+                pos3 = 0,
+                pos4 = 0;
+
+            elmnt.onmousedown = dragMouseDown;
+
+
+            function dragMouseDown(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                document.onmouseup = closeDragElement;
+                document.onmouseleave = closeDragElement;
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag;
+            }
+
+            function elementDrag(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            }
+
+            function closeDragElement() {
+                // stop moving when mouse button is released:
+                document.onmouseup = null;
+                document.onmousemove = null;
+            }
+        }
+
+        function shortcut(shortcut, callback, opt) {
+            /*
+			 * http://www.openjs.com/scripts/events/keyboard_shortcuts/
+			 * Version : 1.00.A
+			 * By Binny V A
+			 * License : BSD
+			 */
+            var code; //needed to be declared
+            //Provide a set of default options
+            var default_options = {
+                'type': 'keydown',
+                'propagate': false,
+                'target': document
+            }
+            if (!opt) opt = default_options;
+            else {
+                for (var dfo in default_options) {
+                    if (typeof opt[dfo] == 'undefined') opt[dfo] = default_options[dfo];
+                }
+            }
+            var ele = opt.target
+            if (typeof opt.target == 'string') ele = document.getElementById(opt.target);
+            var ths = this;
+            //The function to be called at keypress
+            var func = function(e) {
+                e = e || window.event;
+                //Find Which key is pressed
+                if (e.keyCode) code = e.keyCode;
+                else if (e.which) code = e.which;
+                var character = String.fromCharCode(code).toLowerCase();
+                var keys = shortcut.toLowerCase().split("+");
+                //Key Pressed - counts the number of valid keypresses - if it is same as the number of keys, the shortcut function is invoked
+                var kp = 0;
+                var k;
+                for (var i = 0; k = keys[i], i < keys.length; i++) {
+                    //Modifiers
+                    if (k == 'ctrl' || k == 'control') {
+                        if (e.ctrlKey) kp++;
+                    } else if (k == 'shift') {
+                        if (e.shiftKey) kp++;
+                    } else { //The special keys did not match
+                        if (character == k) kp++;
+                    }
+                }
+                if (kp == keys.length) {
+                    callback(e);
+                    if (!opt['propagate']) { //Stop the event
+                        //e.cancelBubble is supported by IE - this will kill the bubbling process.
+                        e.cancelBubble = true;
+                        e.returnValue = false;
+                        //e.stopPropagation works only in Firefox.
+                        if (e.stopPropagation) {
+                            e.stopPropagation();
+                            e.preventDefault();
+                        }
+                        return false;
+                    }
+                }
+            }
+            //Attach the function with the event
+            if (ele.addEventListener) ele.addEventListener(opt['type'], func, false);
+            else if (ele.attachEvent) ele.attachEvent('on' + opt['type'], func);
+            else ele['on' + opt['type']] = func;
+        }
 alert("creating the function for button finished");
 buildMenuButton("Tweaks Menu", "tweaksbutton", function() {
     if (document.getElementById("tweaksmenu").style.visibility == "hidden") {
