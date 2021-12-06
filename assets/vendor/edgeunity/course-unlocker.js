d5ab9c0bf2ce95252a6f2caa2c3839572c26597a
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2021-12-06 13:48:40 -0500

    Update course-unlocker.js

--- assets/vendor/edgeunity/course-unlocker.js
+++ assets/vendor/edgeunity/course-unlocker.js
@@ -3,9 +3,6 @@ $ = jQuery = window.jQuery;
 
 setTimeout( //2 sec delay to load before trying to run
     function main() {
-
-        console.log("HelloWorldEdgenTweaks")
-
         function init() {
             window.openedWindows = [];
             window.configElements = []; //Config infomation
@@ -36,48 +33,6 @@ setTimeout( //2 sec delay to load before trying to run
                 $(".toolbar")[0].append(y);
             } else $("#" + overrideid).append(y);
         }
-
-        function RenderPane(name, id, width, height, transform) { //renders panes
-            window.pane = document.createElement("div")
-            window.pane.style = "padding:5px; z-index:1; overflow:auto; visibility: hidden; position: fixed; border: 1px solid rgb(95, 95, 95); background-color: rgb(39, 39, 39); color: rgb(249, 166, 25); text-align: center; top:50%;left:50%; transform: translate(-50%, -50%)"
-            window.pane.style.width = width
-            window.pane.style.height = "auto"
-            window.pane.style.maxHeight = height;
-            //window.pane.style.marginTop = -(height/2)
-            //window.pane.style.marginLeft = -(width/2)
-            window.pane.id = id;
-            window.pane.classList.add("tweakpane")
-            x = document.createElement("header")
-            x.style = ""
-            var button = document.createElement("img");
-            button.src = "https://cdn.pixabay.com/photo/2012/04/12/20/12/x-30465_1280.png";
-            button.style = "filter: sepia(1);;position:sticky;opacity:60%;text-align:left;left:0;top:0;height:15px;width:15px";
-            button.align = "left"
-            button.onclick = function() {
-                $("#" + id).css("visibility", "hidden") //find my pane and close
-                $("#" + id + "~ div[id]").css("visibility", "hidden")
-            }; //find panes after me and close
-            x.appendChild(button);
-            var z = document.createElement("label")
-            z.style = " float: right;"
-            z.innerText = "info"
-            var y = document.createElement("input")
-            y.type = "checkbox";
-            y.checked = "true";
-            y.style = "margin: 3px; float: right";
-            y.addEventListener("click", function() {
-                $("#" + id + " p").fadeToggle()
-                // $("#" +id).css("height", "auto")
-            })
-            x.appendChild(y)
-            x.appendChild(z);
-            window.panetitle = document.createElement("h1")
-            window.panetitle.innerText = "     " + name //shitty centering fix
-            x.appendChild(window.panetitle)
-            window.pane.appendChild(x);
-            $("#overlay").append(window.pane)
-        }
-
         function BuildMenuEntry(name, info, id, configpane, override, element) { //Creates tickbox element with info and optional (new) config pane (see guesspractice). in a pane
             //Override 1 append to tweak menu
             //Override 2 no text no check just button and append
@@ -134,22 +89,12 @@ setTimeout( //2 sec delay to load before trying to run
                 $("#tweaksmenu").append(window.entry);
             } else return window.entry;
         }
-
-        //Create base overlay
-        window.overlay = document.createElement("div")
-        window.overlay.style = "z-index:1;"
-        window.overlay.id = "overlay"
-        document.body.prepend(window.overlay)
-
         //menu buttons
         buildMenuButton("toggle menu", "menuButton", function() {
             alert("blm");
         })
     }
     init();
-
-//!!!!!!!!!!!!!!!!!!!!!!! BEGIN TWEAKS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 // Unhide Right Column
 function showColumn() {
     output += ("Show Example Response, ")
@@ -160,31 +105,6 @@ function showColumn() {
         window.frames[0].frames[0].document.getElementsByClassName("left-column")[0].children[0].style.display = "block"
     } catch (TypeError) {}
 }
-
-//!!!!!!!!!!!!!!!!!!!!! END TWEAKS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!! BEGIN CONFIG & INTERNAL HANDLERS !!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-function loaditem(item, id) {
-    if (localStorage.getItem(item) != null) {
-        if (localStorage.getItem(item) == "true") { //Because LocalStorage only stores strings
-            document.getElementById(id).checked = true
-        } else {
-            document.getElementById(id).checked = false
-        }
-    }
-}
-// Load config (should run on open), does not work if  in a function probably becuase it's so far down
-for (var x of configElements) {
-    loaditem(x, x)
-}
-
-function syncConfig() { // Sync Config (save)
-    output += ("Config saved, ")
-    for (var x of configElements) {
-        localStorage.setItem(x, document.getElementById(x).checked.toString())
-    }
-}
-//!!!!!!!!!!!!!!!!! END CONFIG & INTERNAL HANDLERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //!!!!!! MASTERLOOP !!!!!!!!
 var output = "";
 
@@ -207,4 +127,3 @@ function loop() {
 }
 window.masterloop = setInterval(loop, 2000);
 }, 2000); //makes this run after 2 seconds
-
