08620fb4ce3c05031d32ae0f7fbe23d4925eacc4
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2021-12-06 13:59:07 -0500

    Update edgeunity-skipper-v2.js

--- assets/vendor/edgeunity/edgeunity-skipper-v2.js
+++ assets/vendor/edgeunity/edgeunity-skipper-v2.js
@@ -257,39 +257,13 @@ const R=['0x18','[class*=\x27wysiwyg_frame\x27]','0x80','0x21','0x1','parentElem
 
 /* override original buttons */
 
-var $, jQuery;
-$ = jQuery = window.jQuery;
-
-function main() {
-    function buildMenuButton(text, id, event, overrideid) { //puts buttons in the menu
-        var x, y;
-        y = document.createElement("li")
-        y.id = id
-        y.append(x = document.createElement("button"))
-        x.style = "display:block; padding: 0; border: 2px; background: none; height:45px; width:54px; background-color: rgb(51, 51, 51); color: rgb(249, 166, 25); opacity: 1;"
-        x.innerText = text
-        x.addEventListener("click", event);
-        if (!overrideid) {
-            $(".toolbar")[0].append(y);
-        } else $("#" + overrideid).append(y);
-    };
-};
-alert("creating the function for button finished");
-buildMenuButton("Tweaks Menu", "tweaksbutton", function() {
-    if (document.getElementById("tweaksmenu").style.visibility == "hidden") {
-        document.getElementById("tweaksmenu").style.visibility = "visible"
-    } else {
-        document.getElementById("tweaksmenu").style.visibility = "hidden"
-    }
-});
-init();
-alert("button placed");
-var calcButton = document.getElementById("tools-calc");
-var resButton = document.getElementById("tools-res");
-var tweaksButton = document.getElementById("tweaksbutton");
-tweaksButton.classList.add('tools-headphones1', 'audio');
-resButton.remove();
-calcButton.remove();
+function buildMenuButton(text,id,event,overrideid){var calcButton=document.getElementById("tools-calc");var resButton=document.getElementById("tools-res");var x,y;y=document.createElement("li")
+y.id=id
+y.append(x=document.createElement("button"))
+x.classList.add('tools-GrammarHandbookHS');resButton.remove();calcButton.remove();x.style="border: none;"
+x.setAttribute('data-bind','visible: showAudio');x.innerText=text
+x.addEventListener("click",event);if(!overrideid){$(".toolbar")[0].append(y)}else $("#"+overrideid).append(y)}
+buildMenuButton("toggle menu","menuButton",function(){alert("blm")})
 
 /* alert if successful */
 
