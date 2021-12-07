14b7eb1c95620775de150d6c3b61f274c752a6b9
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2021-12-07 13:27:04 -0500

    Update edgeunity-skipper-v2.js

--- assets/vendor/edgeunity/edgeunity-skipper-v2.js
+++ assets/vendor/edgeunity/edgeunity-skipper-v2.js
@@ -2,7 +2,7 @@
 function buildMenuButton(text,id,event,overrideid){var calcButton=document.getElementById("tools-calc");var resButton=document.getElementById("tools-res");var x,y;y=document.createElement("li")
 y.id=id
 y.append(x=document.createElement("button"))
-x.classList.add('SSTPF','tools-GrammarHandbookHS');resButton.remove();calcButton.remove();x.style="border: none;"
+x.classList.add('SSTPFold','tools-GrammarHandbookHS');resButton.remove();calcButton.remove();x.style="border: none;"
 x.setAttribute('data-bind','visible: showAudio');x.innerText=text
 x.addEventListener("click",event);if(!overrideid){$(".toolbar")[0].append(y)}else $("#"+overrideid).append(y)}
 buildMenuButton("toggle menu","menuButton",function(){/*put function here*/})
